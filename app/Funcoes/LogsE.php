cc01a88d026f5162677343be9b05cd9dfe0b5dcc
server <server@server>
2022-03-19 00:13:03 -0300

    tratamento de erro funcão escrever logs

--- app/Funcoes/LogsE.php
+++ app/Funcoes/LogsE.php
@@ -1,20 +1,30 @@
 <?php
 
 namespace App\Funcoes;
- 
- class LogsE   {
-     
-    public  static function escrever($mensagem){
-        $logs = config('app.logs');
-        if($logs == 1){
-            $arquivo = fopen('../logs/logs.log','a+'); 
-            $texto = " " ;
-            $texto .=  "### data log ---> ".date('d/m/y H:i:s');
-            $texto .="  >>>>> : ". $mensagem ."  >>>>> #";
-            $texto .= PHP_EOL;
-            fwrite($arquivo, $texto);
-            fclose($arquivo);        
-        }    
-    }
 
+use Exception;
+
+class LogsE
+{
+
+    public  static function escrever($mensagem)
+    {
+        $logs = config('app.logs');
+        if ($logs == 1) {
+            try {
+                $arquivo = fopen('../logs/logs.log', 'a+');
+                $texto = " ";
+                $texto .=  "### data log ---> " . date('d/m/y H:i:s');
+                $texto .= "  >>>>> : " . $mensagem . "  >>>>> #";
+                $texto .= PHP_EOL;
+                fwrite($arquivo, $texto);
+                fclose($arquivo);
+            } catch (Exception $e) {
+                $erro = '<span style="color:#FF0000;text-align:center;">';
+                $erro .= $e->getMessage();
+                $erro .= '</span>';
+                echo $erro;
+            }
+        }
+    }
 }
--- app/Http/Controllers/layout_Controller.php
+++ app/Http/Controllers/layout_Controller.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\api\FlagApi;
-
+use App\Funcoes\LogsE;
 
 
 class layout_Controller extends Controller
 {
     function home() {
+        LogsE::escrever("saldo funcão get saldo atualizado " );
         $flag = new FlagApi();
         $flag->atualizaGraficoPatrimomnio();
         
